a5b831e3c6a4232b2ead561d0a0f232d6818c58b
Ezequiel <ezequiel.e.savedra@gmail.com>
2022-05-26 21:22:53 -0300

    Primer comit de proyecto con despliegue a Heroku

---
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,41 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.persistence.entity.Cliente;
+import com.platzi.market.persistence.entity.Compra;
+import com.platzi.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
+public interface PurchaseMapper {
+
+    @Mappings({
+            @Mapping(source = "idCompra", target = "purchaseId"),
+            @Mapping(source = "idCliente", target = "clientId"),
+            @Mapping(source = "fecha", target = "date"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "productos", target = "items")
+    })
+    Purchase toPurchase(Compra compra);
+
+    List<Purchase> toPurchases(List<Compra> compras);
+
+    //el productos "creo" que no se ignora por el inverse... ojo creo, deberia de averiguarlo bien.
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "cliente", ignore = true)
+    })
+    Compra toCompra(Purchase purchase);
+}
---
+++ src/main/java/com/platzi/market/web/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.platzi.market.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+//esta anotacion no va mas
+// la version actual de springfox-swagger-ui es la 3.0.0
+// en el curso se usaba la 2.9.2
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.platzi.market.web.controller"))
+                .build();
+    }
+}
